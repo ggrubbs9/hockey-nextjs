1beafbb4c597f2e74a29601a98e2b824be1bdb48
ggrubbs1 <36514251+ggrubbs9@users.noreply.github.com>
2025-06-30 14:11:18 -0400

    init

---
+++ hockey-nextjs/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import Navbar from "../components/navbar";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}

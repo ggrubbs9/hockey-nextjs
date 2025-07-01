081f200a4c5501f3f774cc753c959180ca5917de
ggrubbs1 <36514251+ggrubbs9@users.noreply.github.com>
2025-07-01 11:46:40 -0400

    search set up

--- hockey-nextjs/pages/_app.tsx
+++ hockey-nextjs/pages/_app.tsx
@@ -1,13 +1,23 @@
 import type { AppProps } from "next/app";
 import Navbar from "../components/navbar";
+import "../globals.css";
+import "@mantine/core/styles.css";
+
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <MantineProvider theme={theme}>
+        <Navbar />
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </MantineProvider>
     </>
   );
 }
--- hockey-nextjs/pages/index.tsx
+++ hockey-nextjs/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>Welcome Home</h1>;
+import { Input } from "@mantine/core";
+import { getHotkeyHandler } from "@mantine/hooks";
+import { IconAt } from "@tabler/icons-react";
+import { useState } from "react";
+
+export default function Search() {
+  // use endpoint to grab player id:
+  // https://search.d3.nhle.com/api/v1/search/player?culture=en-us&limit=20&q=kane
+
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    console.log(value);
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to Search</h1>
+      <Input
+        placeholder="Your email"
+        onChange={(event) => setValue(event.target.value)}
+        onKeyDown={getHotkeyHandler([["Enter", handleSubmit]])}
+        leftSection={<IconAt size={16} />}
+      />
+    </div>
+  );
 }
